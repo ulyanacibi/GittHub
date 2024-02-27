4f7bae28a2d0cfd979a2f87a233157eca3490973
Ульяна <149614568+ulyanacibi@users.noreply.github.com>
2024-02-27 10:54:05 +0300

    Update Program.cs
    Задание готово к проверке.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿int x;
 Console.WriteLine("Введите число");
 x=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"y={(Math.Sqrt(2))*(Math.Pow(x,2))}");
+Console.WriteLine($"y={(Math.Sqrt(2))*(Math.Pow(x,2))/2}");
 
