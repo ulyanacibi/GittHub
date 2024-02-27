22c4ff75e86d2ec7ba26c935f4503b7f023d5618
Ульяна <149614568+ulyanacibi@users.noreply.github.com>
2024-02-27 11:04:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿int x;
 Console.WriteLine("Введите число");
 x=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"y={(Math.Sqrt(2))*(Math.Pow(x,2))}");
-
+Console.WriteLine($"y={(Math.Sqrt(Math.Pow(x, 2)))}");
